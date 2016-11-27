91e11fcd4636f5054b386de57d157ee658971e30
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-27 04:56:40 -0500

    package skeleton

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Convert MMWR weeks <--> dates
+
+
+/**
+ * Return javascript date object for Sunday of given week
+ */
+export const MMWRWeekToDate = (year, week) => {
+  return null
+}
+
+/**
+ * Convert given date (default now) to MMWRWeek
+ */
+export const DateToMMWRWeek = (date = new Date()) => {
+  return null
+}
+
+/**
+ * Return the number of MMWR weeks in a year
+ */
+export const MMWRWeeksInYear = year => {
+  return null
+}
