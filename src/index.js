eac8f4be99dd83c2799dd3784af6d0ca57e04413
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-27 21:49:34 -0500

    Minimal port of MMWRWeek.R

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
 // Convert MMWR weeks <--> dates
 
+import moment from 'moment'
 
 /**
- * Return javascript date object for Sunday of given week
+ * Return start date of the year using MMWR week rules
  */
-export const MMWRWeekToDate = (year, week) => {
-  return null
+const startDateOfYear = year => {
+  let janOne = moment(year + '0101')
+  let diff = 7 * (janOne.weekday() > 3) - janOne.weekday()
+  return janOne.add(diff, 'days')
 }
 
 /**
- * Convert given date (default now) to MMWRWeek
+ * Return date for Sunday (default) of given week
  */
-export const DateToMMWRWeek = (date = new Date()) => {
-  return null
+export const MMWRWeekToDate = (year, week, day = 0) => {
+  let dayOne = startDateOfYear(year)
+  let diff = 7 * (week - 1) + day
+  return dayOne.add(diff, 'days')
+}
+
+/**
+ * Convert given moment date (default: now) to MMWRWeek
+ */
+export const DateToMMWRWeek = (date = moment()) => {
+  let year = date.year()
+  let startDates = [year - 1, year, year + 1]
+      .map(y => startDateOfYear(y))
+  let diffs = startDates.map(d => date.diff(d))
+
+  let startId = 1
+  if (diffs[1] < 0) startId = 0
+  else if (diffs[2] >= 0) startId = 2
+
+  let startDate = startDates[startId]
+
+  return {
+    year: moment(startDate).add(7, 'days').year(),
+    week: Math.floor(date.diff(startDate, 'days') / 7) + 1,
+    day: date.weekday()
+  }
 }
 
 /**
