80e5abb499422734ae66e77428f31da908633bb6
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-27 22:21:27 -0500

    Assertions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // Convert MMWR weeks <--> dates
 
 import moment from 'moment'
+import chai from 'chai'
+
+chai.should()
 
 /**
  * Return start date of the year using MMWR week rules
  */
-const startDateOfYear = year => {
+export const startDateOfYear = year => {
+  year.should.be.a('number')
+
   let janOne = moment(year + '0101')
   let diff = 7 * (janOne.weekday() > 3) - janOne.weekday()
   return janOne.add(diff, 'days')
@@ -15,6 +20,12 @@ const startDateOfYear = year => {
  * Return date for Sunday (default) of given week
  */
 export const MMWRWeekToDate = (year, week, day = 0) => {
+  year.should.be.a('number')
+  week.should.be.a('number')
+  day.should.be.a('number')
+  week.should.be.within(1, 53)
+  day.should.be.within(0, 6)
+
   let dayOne = startDateOfYear(year)
   let diff = 7 * (week - 1) + day
   return dayOne.add(diff, 'days')
@@ -24,6 +35,8 @@ export const MMWRWeekToDate = (year, week, day = 0) => {
  * Convert given moment date (default: now) to MMWRWeek
  */
 export const DateToMMWRWeek = (date = moment()) => {
+  date.should.be.an.instanceof(moment)
+
   let year = date.year()
   let startDates = [year - 1, year, year + 1]
       .map(y => startDateOfYear(y))
@@ -46,5 +59,7 @@ export const DateToMMWRWeek = (date = moment()) => {
  * Return the number of MMWR weeks in a year
  */
 export const MMWRWeeksInYear = year => {
+  year.should.be.a('number')
+
   return DateToMMWRWeek(MMWRWeekToDate(year, 53)).year === year ? 53 : 52
 }
