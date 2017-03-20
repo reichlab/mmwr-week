d0a39d529e2ada6d42a0521c224651723c1432a4
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-03-20 00:45:39 -0400

    Single class for everything

--- index.ts
+++ index.ts
@@ -2,101 +2,115 @@
 
 import * as moment from 'moment'
 
-interface MMWRWeek {
-  year: number,
-  week: number,
-  day?: number
-}
+export class MMWRDate {
+  constructor (public year: number, public week?: number, public day?: number) {}
 
-/**
- * Return start date of the year using MMWR week rules
- */
-function startDateOfYear (year: number) {
-  let janOne: any = moment(year + '0101')
-  let diff = 7 * +(janOne.isoWeekday() > 3) - janOne.isoWeekday()
-  return janOne.add(diff, 'days')
-}
-
-/**
- * Return date for given week (starts at Sunday)
- */
-export function MMWRWeekToDate (mweek: MMWRWeek) {
-  let dayOne = startDateOfYear(mweek.year)
-  let diff = 7 * (mweek.week - 1)
-  if (mweek.day) {
-    diff += mweek.day - 1
+  /**
+   * Return year start moment date
+   */
+  get startMomentDate () {
+    let janOne: any = moment(this.year + '0101')
+    let diff = 7 * +(janOne.isoWeekday() > 3) - janOne.isoWeekday()
+    return janOne.add(diff, 'days')
   }
-  return dayOne.add(diff, 'days')
-}
 
-/**
- * Return MMWR week with given week delta
- */
-export function MMWRWeekWithDelta (mweek: MMWRWeek, delta: number): MMWRWeek {
-  // delta.should.be.within(-52, 52) Don't care after that. FIXME
-  let out = mweek.week + delta
-  if (delta > 0) {
-    let maxWeek = MMWRWeeksInYear(mweek.year)
-    return {
-      year: out > maxWeek ? mweek.year + 1 : mweek.year,
-      week: out > maxWeek ? out - maxWeek : out
+  /**
+   * Return a moment representation
+   */
+  toMomentDate () {
+    let dayOne = this.startMomentDate
+    let diff = 7 * (this.week - 1)
+    if (this.day) {
+      diff += this.day - 1
     }
-  } else {
-    let maxWeek = MMWRWeeksInYear(mweek.year - 1)
-    return {
-      year: out < 1 ? mweek.year - 1 : mweek.year,
-      week: out < 1 ? maxWeek + out : out
+    return dayOne.add(diff, 'days')
+  }
+
+  /**
+   * Set values using given moment date
+   */
+  fromMomentDate (date = moment()) {
+    let year = date.year()
+    let startDates = [year - 1, year, year + 1]
+      .map(y => {
+        let md = new MMWRDate(y)
+        return md.startMomentDate
+      })
+    let diffs = startDates.map(d => date.diff(d))
+
+    let startId = 1
+    if (diffs[1] < 0) startId = 0
+    else if (diffs[2] >= 0) startId = 2
+
+    let startDate = startDates[startId]
+
+    this.year = moment(startDate).add(7, 'days').year()
+    this.week = Math.floor(date.diff(startDate, 'days') / 7) + 1
+    this.day = (date.isoWeekday() % 7) + 1
+  }
+
+  /**
+   * Number of weeks in this MMWR season
+   */
+  get nWeeks (): number {
+    let md = new MMWRDate(this.year, 53)
+    md.fromMomentDate(md.toMomentDate())
+    return md.year === this.year ? 53 : 52
+  }
+
+  /**
+   * Return number of weeks differing from this
+   */
+  diffWeek (mdate: MMWRDate): number {
+    if (this.year === mdate.year) {
+      return this.week - mdate.week
+    } else {
+      // Order of dates [low, high]
+      let ds = [this, mdate]
+      let sign = -1
+      if (this.year > mdate.year) {
+        ds = [mdate, this]
+        sign = 1
+      }
+
+      let diff = ds[1].week + ds[0].nWeeks - ds[0].week
+
+      let begin = ds[0].year + 1
+      while (begin < ds[1].year) {
+        diff += ds[0].nWeeks
+        begin++
+      }
+      return sign * diff
+    }
+  }
+
+  /**
+   * Apply week delta
+   */
+  applyWeekDiff (delta: number) {
+    let newWeek
+
+    while (true) {
+      newWeek = this.week + delta
+      if (delta > 0) {
+        if (newWeek > this.nWeeks) {
+          delta = newWeek - this.nWeeks - 1
+          this.week = 1
+          this.year++
+        } else {
+          this.week = newWeek
+          break
+        }
+      } else {
+        if (newWeek <= 0) {
+          this.year--
+          this.week = this.nWeeks
+          delta = newWeek
+        } else {
+          this.week = newWeek
+          break
+        }
+      }
     }
   }
 }
-
-/**
- * Convert given moment date (default: now) to MMWRWeek
- */
-export function DateToMMWRWeek (date = moment()): MMWRWeek {
-  let year = date.year()
-  let startDates = [year - 1, year, year + 1]
-      .map(y => startDateOfYear(y))
-  let diffs = startDates.map(d => date.diff(d))
-
-  let startId = 1
-  if (diffs[1] < 0) startId = 0
-  else if (diffs[2] >= 0) startId = 2
-
-  let startDate = startDates[startId]
-
-  return {
-    year: moment(startDate).add(7, 'days').year(),
-    week: Math.floor(date.diff(startDate, 'days') / 7) + 1,
-    day: (date.isoWeekday() % 7) + 1
-  }
-}
-
-/**
- * Return the number of MMWR weeks in a year
- */
-export function MMWRWeeksInYear (year: number): number {
-  return DateToMMWRWeek(MMWRWeekToDate({
-    year: year,
-    week: 53
-  })).year === year ? 53 : 52
-}
-
-/**
- * Return number of weeks between given MMWR weeks
- */
-export function MMWRWeekDiff (mweekLow: MMWRWeek, mweekHigh: MMWRWeek): number {
-  if (mweekLow.year === mweekHigh.year) {
-    return mweekHigh.week - mweekLow.week
-  } else {
-    let diff = mweekHigh.week + (MMWRWeeksInYear(mweekLow.year) - mweekLow.week)
-
-    let low = mweekLow.year + 1
-    while (low < mweekHigh.year) {
-      diff += MMWRWeeksInYear(low)
-      low++
-    }
-    return diff
-  }
-}
-
